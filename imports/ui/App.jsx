0ddf8423dd82a7f5e2addbe971b6482b56c85604
SHRicard <SH_Ricardo@hotmail.com>
2022-04-07 20:27:07 -0300

    meteor inicio  v2

---
+++ imports/ui/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useTracker } from "meteor/react-meteor-data";
+import { Hello } from "./Hello.jsx";
+import { Info } from "./Info.jsx";
+import { Task } from "./Task.jsx";
+
+const tasks = [
+  { _id: 1, text: "First Task" },
+  { _id: 2, text: "Second Task" },
+  { _id: 3, text: "Third Task" },
+];
+export const App = () => (
+  <div>
+    <h1>Welcome to Meteor!</h1>
+    <Hello />
+    <Info />
+    <ul>
+      {tasks.map((task) => (
+        <Task key={task._id} task={task} />
+      ))}
+    </ul>
+  </div>
+);
